0e7b02d185062bb00f6405e8817440d73534e949
Maks <maks.kuv58@gmail.com>
2022-10-28 22:31:57 +0300

    Create project_2.py

---
+++ project_2.py
@@ -0,0 +1,10 @@
+def task_2():
+    with open('/home/stud/Desktop/ico.desktop', 'w+', encoding='utf8') as f:
+        f.write('[Desktop Entry]\n'
+                'Name=Политех\n'
+                'GenericName=Политехническйи\n'
+                'Comment=Институт\n'
+                'URL=https://politeh.pnzgu.ru\n'
+                'Type=Link\n'
+                'Icon=text-html\n'
+                'Terminal=false')
